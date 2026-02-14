98c35726814bd24bf115f91fe320c80ed3ec8b19
Josef Knotek <adolf7890@gmail.com>
2026-02-14 23:00:43 +0100

    old high school work

---
+++ pcv/slovnik java/dictionary/databaze moje/slovnik.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Úte 27. úno 2018, 09:25
+-- Verze serveru: 5.6.24
+-- Verze PHP: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databáze: `slovnik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `slovnicek`
+--
+
+CREATE TABLE IF NOT EXISTS `slovnicek` (
+  `id` int(11) NOT NULL,
+  `cs` varchar(50) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
+  `en` varchar(50) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
+  `slovni_druh` varchar(15) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Vypisuji data pro tabulku `slovnicek`
+--
+
+INSERT INTO `slovnicek` (`id`, `cs`, `en`, `slovni_druh`) VALUES
+(1, 'počítač', 'computer', 'podstatné jméno'),
+(4, 'plamen', 'flame', 'podstatné jméno'),
+(6, 'oheň', 'fire', 'podstatné jméno'),
+(7, 'slovo', 'word', ''),
+(8, 'léto', 'summer', ''),
+(9, 'stůl', 'table', ''),
+(10, 'a', 'and', 'spojka');
+
+--
+-- Klíče pro exportované tabulky
+--
+
+--
+-- Klíče pro tabulku `slovnicek`
+--
+ALTER TABLE `slovnicek`
+  ADD PRIMARY KEY (`id`), ADD KEY `slovnicek_cs_idx` (`cs`), ADD KEY `slovnicek_en_idx` (`en`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `slovnicek`
+--
+ALTER TABLE `slovnicek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
